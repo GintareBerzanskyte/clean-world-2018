749a825195407256561638e41626f62af5c6daf2
Gintare <gintarekotryna@gmail.com>
2018-12-01 11:55:50 +0200

    commit info activity

--- app/src/main/java/com/example/gintare/cleanworld/MainActivity.java
+++ app/src/main/java/com/example/gintare/cleanworld/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.gintare.cleanworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void info(View view) {
+        Intent info = new Intent(MainActivity.this, info.class);
+        startActivity(info);
     }
 }
