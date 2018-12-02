6f8fc2ff14cc3b5c6193fe9b4fb27d3cee704db3
Gintare <gintarekotryna@gmail.com>
2018-12-02 08:47:21 +0200

    final update

--- app/src/main/java/com/example/gintare/cleanworld/DIYActivity.java
+++ app/src/main/java/com/example/gintare/cleanworld/DIYActivity.java
@@ -16,9 +16,29 @@ public class DIYActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diy);
 
-        WebView myWebView = (WebView) findViewById(R.id.web1);
-        WebSettings webSettings = myWebView.getSettings();
+        WebView my1 = (WebView) findViewById(R.id.web1);
+        WebSettings webSettings = my1.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://www.youtube.com/embed/mzmzk_q0LR4");
+        my1.loadUrl("https://www.youtube.com/embed/mzmzk_q0LR4");
+
+        WebView my2 = (WebView) findViewById(R.id.web2);
+        WebSettings webSettings2 = my2.getSettings();
+        webSettings2.setJavaScriptEnabled(true);
+        my2.loadUrl("https://www.youtube.com/embed/XJm0r_lMUXo");
+
+        WebView my3 = (WebView) findViewById(R.id.web3);
+        WebSettings webSettings3 = my3.getSettings();
+        webSettings3.setJavaScriptEnabled(true);
+        my3.loadUrl("https://www.youtube.com/embed/lygBFntOyUE");
+
+        WebView my4 = (WebView) findViewById(R.id.web4);
+        WebSettings webSettings4 = my4.getSettings();
+        webSettings4.setJavaScriptEnabled(true);
+        my4.loadUrl("https://www.youtube.com/embed/IE_KqcMoeS0");
+
+        WebView my5 = (WebView) findViewById(R.id.web5);
+        WebSettings webSettings5 = my5.getSettings();
+        webSettings5.setJavaScriptEnabled(true);
+        my5.loadUrl("https://www.youtube.com/embed/urL1kKOBeqg");
     }
 }
--- app/src/main/java/com/example/gintare/cleanworld/MainActivity.java
+++ app/src/main/java/com/example/gintare/cleanworld/MainActivity.java
@@ -3,20 +3,54 @@ package com.example.gintare.cleanworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] DATA = new String[] {
+            "Vandens buteliukas","Vandens butelys","Stiklainis","Gerimo buteliukas","Stiklinis butelis Borjomi","Stiklinis butelis Vichhy","Žalio stiklo butelis","Heineken butelis","Tuborg butelis","Stiklinis giros butelis 'Duonos Gira'","Stiklinis giros butelis'Smetoni�ka Gira'",
+            "Pakavimo deže","Pusryciu dribsniu dežute 'Cookie Crisp'","Sveikinimu atvirute","Siuntiniu deže","Sausainiu dežute","šaldyto maisto dežute","Arbatos dežute","Laikraštis","Pusryciu dribsniu dežute 'Fitness'","Vokas","Rašomasis popierius",
+            "Mineralinio vandens buteliukas 'Vytautas'","Mineralinio vandens buteliukas 'Akvile'","Mineralinio vandens buteliukas 'Rasa'","Šalta arbata 'NESTEA'","Mineralinio vandens buteliukas 'Neptunas'","Butelio kamštelis","Nespalvotas sulciu butelis 'Tymbark'","Ledai plastikiniame indelyje 'Aurum'","Plastikinis maišelis","Išsinešimui skirtu gerimu puodeliai","Puodelis kudikiams","Buteliukas kudikiams","Tepamo surio pakuote","Kiaušiniu deklai","Išsinešamo maisto dežute","Vienkartiniai karštu gerimu puodeliai",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AutoCompleteTextView searchAutoComplete = (AutoCompleteTextView) findViewById(R.id.editText2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.custom_list_item,
+                R.id.text_view_list_item, DATA);
+        searchAutoComplete.setAdapter(adapter);
+
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ""+parent.getItemAtPosition((position)), Toast.LENGTH_SHORT).show();
+
+                // start new activity with params
+//                Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
+//                myIntent.putExtra("firstKeyName","FirstKeyValue");
+//                myIntent.putExtra("secondKeyName","SecondKeyValue");
+//                startActivity(myIntent);
+
+                // get data
+//                Intent myIntent = getIntent(); // gets the previously created intent
+//                String firstKeyName = myIntent.getStringExtra("firstKeyName"); // will return "FirstKeyValue"
+//                String secondKeyName= myIntent.getStringExtra("secondKeyName");
+            }
+        });
+
     }
 
     public void info(View view) {
-        Intent info = new Intent(MainActivity.this, info.class);
+        Intent info = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(info);
     }
     public void diy(View view) {
